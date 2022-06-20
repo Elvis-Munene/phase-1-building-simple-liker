2081876d45621871c27bc340e1b6d7aa470e37b3
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-06-20 03:53:10 +0300

    Done.

--- main.js
+++ main.js
@@ -5,6 +5,39 @@ const FULL_HEART = '♥'
 // Your JavaScript code goes here!
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  const loveHearts = document.querySelectorAll('span.like-glyph')
+  
+  loveHearts.forEach(loveHearts => loveHearts.addEventListener('click', function (e){
+    mimicServerCall()
+    .then(function handleHearLike(){
+      if(e.target.textContent === EMPTY_HEART){
+
+      loveHearts.classList.add("activated-heart");
+      loveHearts.textContent = FULL_HEART;
+
+     } 
+     else if(e.target.textContent === FULL_HEART){
+      loveHearts.textContent = EMPTY_HEART;
+      loveHearts.classList.remove('activated-heart')
+     }
+    })
+    .catch((response)=>{
+      errorModal.classList.remove('hidden');
+      setTimeout(function(){
+        errorModal.classList.add('hidden')
+      }, 3000);
+    })
+  }))
+
+})
+
+
+
+
+
+
+
 
 
 //------------------------------------------------------------------------------
